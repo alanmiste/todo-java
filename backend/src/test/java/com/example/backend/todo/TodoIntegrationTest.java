d684cfdaa954d40e1c6e5722c6c9dba86d8bbd43
Alan <baranalaan@gmail.com>
2022-07-16 20:40:02 +0200

    update TodoIntegrationTest- add two POST and one GET

--- backend/src/test/java/com/example/backend/todo/TodoIntegrationTest.java
+++ backend/src/test/java/com/example/backend/todo/TodoIntegrationTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,8 +21,50 @@ public class TodoIntegrationTest {
 
     @Test
     void listTodo() throws Exception {
-        mockMvc.perform(
-                get("/api/todo")
-        ).andExpect(status().isOk());
+        //Given
+        mockMvc
+
+                //When
+                .perform(
+                        post("/api/todo")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "description":"go to Moon",
+                                            "status":"OPEN"
+                                        }
+                                        """)
+                )
+                //Then
+                .andExpect(status().isOk());
+
+        mockMvc
+                .perform(
+                        post("/api/todo")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                        "description":"go to Mars",
+                                        "status":"OPEN"
+                                        }
+                                        """)
+                )
+                        .andExpect(status().isOk());
+
+        mockMvc
+
+                .perform(
+                        get("/api/todo")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            [{
+                            "description":"go to Moon",
+                            "status":"OPEN"},
+                            {
+                            "description": "go to Mars",
+                            "status": "OPEN"
+                            }]
+                        """));
     }
 }

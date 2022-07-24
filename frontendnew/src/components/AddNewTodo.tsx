babc79ef0d650d954b08e49ae73a4265f48347d2
Alan <baranalaan@gmail.com>
2022-07-24 15:44:29 +0200

    add reset input after adding new Todo

--- frontendnew/src/components/AddNewTodo.tsx
+++ frontendnew/src/components/AddNewTodo.tsx
@@ -5,15 +5,20 @@ export default function AddNewTodo(props: {getAllTodos : ()=>void}){
     const [todoDescription, setTodoDescription]= useState<string>("")
 
     const postTodo = (description : string) =>{
-        axios.post("/api/todo",{"description":description, "status": "OPEN"})
-            .then(response => console.log(response))
-            .then(props.getAllTodos)
-            .catch(error => console.log(error))
+        if(description !="") {
+            axios.post("/api/todo", {"description": description, "status": "OPEN"})
+                .then(response => console.log(response))
+                .then(props.getAllTodos)
+                .catch(error => console.log(error))
+            setTodoDescription('')
+        }else {
+            alert("Please enter a task!")
+        }
 
     }
     return(
         <div>
-            <input id={"inputTodo"} type="text" onChange={event => setTodoDescription(event.target.value)}/>
+            <input id={"inputTodo"} type="text" value={todoDescription} onChange={event => setTodoDescription(event.target.value)}/>
             <button onClick={()=>postTodo(todoDescription)}>Add</button>
         </div>
     )

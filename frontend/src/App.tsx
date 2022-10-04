e83a1aee6d40b39033a277b9871ddab9e0cd7bc6
alanmiste <baranalaan@gmail.com>
2022-10-04 20:30:26 +0200

    Update TodoLists CSS, Change font family.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
-import TodoLists from "./components/TodoLists";
-import AddNewTodo from "./components/AddNewTodo";
 import useTodos from "./hooks/useTodos";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Open from "./pages/Open";
 import InProgress from "./pages/InProgress";
 import Done from "./pages/Done";
-import NavigationBar from "./components/NavigationBar";
-
 
 function App() {
 
-    //Destructuring, what came from useTodos
     const {todos, delTodo , getAllTodos, postTodo, changeStatus } = useTodos()
 
     return (
         <div className="App">
             <HashRouter>
                 <Header/>
-
-
                 <Routes>
                     <Route path={"/"} element={<Home todos={todos} getAllTodos={getAllTodos} changeStatus={changeStatus} delTodo={delTodo} postTodo={postTodo} />} />
                     <Route path={"/open"} element={<Open todos={todos} getAllTodos={getAllTodos} changeStatus={changeStatus} delTodo={delTodo}/>} />

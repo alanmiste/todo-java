d90088cda305feca8a1169c33a8269b0b32ed915
Alan Miste <baranalaan@gmail.com>
2024-07-02 16:51:09 +0200

    Add burger navbar.

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,27 +1,35 @@
-import {NavLink} from "react-router-dom";
-import React, {useState} from "react";
-import "./NavigationBar.css"
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import "./NavigationBar.css";
 
 export default function NavigationBar() {
+    const [navClass, setNavClass] = useState("topNav");
 
-    const [navClass , setNavClass] = useState<string>("topNav");
     const myFunction = () => {
-
         if (navClass === "topNav") {
             setNavClass("topNav responsive");
         } else {
             setNavClass("topNav");
         }
-    }
+    };
+
     return (
         <nav className={navClass}>
             <a href="#" className="icon" onClick={myFunction}>
-                <i>=</i>
+                &#9776; {/* Unicode for the burger menu icon */}
             </a>
-            <a><NavLink to={"/"} >Home</NavLink></a>
-            <a><NavLink to={"/open"}>Open</NavLink></a>
-            <a><NavLink to={"/inprogress"}>In Progress</NavLink></a>
-            <a><NavLink to={"/done"}>Done</NavLink></a>
+            <NavLink to="/">
+                Home
+            </NavLink>
+            <NavLink to="/open">
+                Open
+            </NavLink>
+            <NavLink to="/inprogress">
+                In Progress
+            </NavLink>
+            <NavLink to="/done">
+                Done
+            </NavLink>
         </nav>
-    )
+    );
 }
